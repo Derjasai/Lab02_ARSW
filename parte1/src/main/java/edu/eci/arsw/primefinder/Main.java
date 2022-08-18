07d154fed1708eb6c41e4b26532903ed2158b0e3
unknown <daniel.ramos-j@labinfo.is.escuelaing.edu.co>
2022-08-18 09:17:32 -0500

    prime avance lab

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,11 +3,31 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
 	public static void main(String[] args) {
+		long s = System.currentTimeMillis();
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 10000000);
+//		PrimeFinderThread pft2=new PrimeFinderThread(10000001, 20000000);
+//		PrimeFinderThread pft3=new PrimeFinderThread(20000001, 30000000);
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
+		pft1.start();
+//		pft2.start();
+//		pft3.start();
+
+
+		synchronized (pft1){
+			long e = System.currentTimeMillis();
+			double time = e - s;
+			while (time <= (long) 500.0){
+				System.out.println(time);
+				try {
+					pft1.wait();
+				} catch (InterruptedException ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		}
+		long ef = System.currentTimeMillis();
+		double time = ef - s;
+		System.out.println("time" + time);
 		
 	}
 	
